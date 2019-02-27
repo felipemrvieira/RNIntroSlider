5d8c55f9817746aecd55b2032ed9a469410ed991
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2019-02-27 17:58:14 -0300

    Ajusta layout

--- Intro.js
+++ Intro.js
@@ -5,81 +5,126 @@ import App from './App';
 
 
 const styles = StyleSheet.create({
-    mainContent: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-    },
-    image: {
-        width: 250,
-        height: 250,
-    },
-    text: {
-        color: '#424242',
-        backgroundColor: 'transparent',
-        textAlign: 'center',
-        paddingHorizontal: 16,
-    },
-    title: {
-        fontSize: 28,
-        color: '#424242',
-        backgroundColor: 'transparent',
-        textAlign: 'center',
-        marginBottom: 16,
-    }
+  mainContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  image: {
+    width: 220,
+    height: 220,
+  },
+  text: {
+    color: '#7b7b7b',
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    paddingHorizontal: 16,
+    
+  },
+  title: {
+    fontSize: 22,
+    color: '#424242',
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    marginBottom: 16,
+    
+
+  }
 });
 
 const slides = [
   {
     key: 'somethun',
     title: 'Title 1',
+    titleStyle: styles.title,
     text: 'Description.\nSay something cool',
+    textStyle: styles.text,
     image: require('./assets/1.png'),
     imageStyle: styles.image,
     backgroundColor: '#f6f6f6',
   },
   {
-    key: 'somethun-dos',
-    title: 'Title 2',
-    text: 'Other cool stuff',
+    key: 'somethun',
+    title: 'Title 1',
+    titleStyle: styles.title,
+    text: 'Description.\nSay something cool',
+    textStyle: styles.text,
     image: require('./assets/2.png'),
     imageStyle: styles.image,
     backgroundColor: '#f6f6f6',
   },
   {
-    key: 'somethun1',
-    title: 'Rocket guy',
-    text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+    key: 'somethun',
+    title: 'Title 1',
+    titleStyle: styles.title,
+    text: 'Description.\nSay something cool',
+    textStyle: styles.text,
     image: require('./assets/3.png'),
     imageStyle: styles.image,
     backgroundColor: '#f6f6f6',
   }
 ];
 
+
 export default class Intro extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-          showRealApp: false
-        }
-    }
-  _onDone = () => {
-    // User finished the introduction. Show real app through
-    // navigation or simply by controlling state
-    this.setState({ showRealApp: true });
-  }
+  _renderItem = props => (
+    <LinearGradient
+    style={[styles.mainContent, {
+      paddingTop: props.topSpacer,
+      paddingBottom: props.bottomSpacer,
+      width: props.width,
+      height: props.height,
+    }]}
+    colors={props.colors}
+    start={{x: 0, y: .1}} end={{x: .1, y: 1}}
+    >
+      {/* <Ionicons style={{ backgroundColor: 'transparent' }} name={props.icon} size={150} color="white" /> */}
+      <View>
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.text}>{props.text}</Text>
+      </View>
+    </LinearGradient>
+  );
+  
   render() {
-    if (this.state.showRealApp) {
-      return <App />;
-    } else {
-      return <AppIntroSlider 
-      slides={slides} 
-      onDone={this._onDone} 
+    return (
+      <AppIntroSlider
+      slides={slides}
+      // renderItem={this._renderItem}
+      bottomButton
       skipLabel='Pular'
       doneLabel='Pronto' 
       nextLabel='Próximo' 
       prevLabel='Anterior'
-      />;
+      />
+      );
     }
   }
-}
+
+// export default class Intro extends React.Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//           showRealApp: false
+//         }
+//     }
+//   _onDone = () => {
+//     // User finished the introduction. Show real app through
+//     // navigation or simply by controlling state
+//     this.setState({ showRealApp: true });
+//   }
+//   render() {
+//     if (this.state.showRealApp) {
+//       return <App />;
+//     } else {
+//       return <AppIntroSlider 
+//       slides={slides} 
+//       onDone={this._onDone} 
+//       skipLabel='Pular'
+//       doneLabel='Pronto' 
+//       nextLabel='Próximo' 
+//       prevLabel='Anterior'
+//       />;
+//     }
+//   }
+// }
